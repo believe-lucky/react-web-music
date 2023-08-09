b6473fc3b806e665fe7383fa1a41bf77a2038af3
qiliangliang <qiliangliang@rhtect.com>
2023-08-09 18:20:21 +0800

    feat(config):添加配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+declare module "*.less" {
+  const resource: { [key: string]: string };
+  export = resource;
+}
+declare module "*.module.less" {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+
+declare module "@*";
