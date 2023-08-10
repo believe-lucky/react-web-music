2fc18960908ed9f9d9f2f9bac90073ccf7b071cb
qiliangliang <qiliangliang@rhtect.com>
2023-08-10 16:44:52 +0800

    feat(格式化)：格式化测试

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,40 +1,40 @@
-import { Layout, } from 'antd';
-import './layout.less'
+import { Layout } from "antd";
+import "./layout.less";
 
-const { Header, Sider, Content, Footer} = Layout
+const { Header, Sider, Content, Footer } = Layout;
 
-import HeaderContainer from './components/Header';
+import HeaderContainer from "./components/Header";
 const contentStyle: React.CSSProperties = {
-  textAlign: 'center',
+  textAlign: "center",
   minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#108ee9',
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9",
 };
 
 const siderStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#3ba0e9',
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9",
 };
 
 const footerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#7dbcea',
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#7dbcea",
 };
 export default function LayoutContainer() {
   return (
-    <Layout className='layoutBox'>
-      <Header id='headerContainer'>
-        <HeaderContainer/>
+    <Layout className="layoutBox">
+      <Header id="headerContainer">
+        <HeaderContainer />
       </Header>
-      <Layout hasSider className='layoutContent'>
+      <Layout hasSider className="layoutContent">
         <Sider style={siderStyle}>Sider</Sider>
         <Content style={contentStyle}>Content</Content>
       </Layout>
       <Footer style={footerStyle}>Footer</Footer>
     </Layout>
-  )
+  );
 }
