e5d00a616f3c1044dda1190abbb8dbe2b12a85ee
lucky <lucky@lucky.com>
2023-08-14 10:07:59 +0800

    feat(L2Dwidget):链接不稳定，改成npm

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import "./assets/css/index.css";
 import "./assets/css/iconfont.css";
 import "@/styles/index.less";
+import "@/plugins/L2Dwidget";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
