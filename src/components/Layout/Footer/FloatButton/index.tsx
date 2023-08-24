2b6b2c544f1068d841fdee36d8eb6e996db14a34
qiliangliang <qiliangliang_email@163.com>
2023-08-24 13:51:04 +0800

    feat(console)：log

--- src/components/Layout/Footer/FloatButton/index.tsx
+++ src/components/Layout/Footer/FloatButton/index.tsx
@@ -2,9 +2,14 @@ import {
   GithubFilled,
   ZhihuCircleFilled,
   CustomerServiceOutlined,
+  SendOutlined,
 } from "@ant-design/icons";
 import React from "react";
 import { FloatButton } from "antd";
+// import juejin from "@/assets/images/juejin.png";
+// const HeartIcon = (props: Partial<CustomIconComponentProps>) => (
+//   <Icon component={HeartSvg} {...props} />
+// );
 const MusicIcon: React.FC = () => (
   <>
     <FloatButton.Group
@@ -13,6 +18,11 @@ const MusicIcon: React.FC = () => (
       style={{ right: 30, bottom: 380 }}
       icon={<CustomerServiceOutlined />}
     >
+      <FloatButton
+        href="https://juejin.cn/"
+        icon={<SendOutlined />}
+        target="_blank"
+      />
       <FloatButton
         href="https://www.zhihu.com/"
         target="_blank"
