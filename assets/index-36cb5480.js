7d9d88e1a0b1d526f8520b72cdc9b616b2979db5
qiliangliang <qiliangliang_email@163.com>
2023-08-30 17:34:22 +0800

    Updates

--- assets/index-36cb5480.js
+++
@@ -1,1 +0,0 @@
-import{az as t}from"./index-584936be.js";function i(){return t.jsx("div",{children:"setting"})}export{i as default};
---
+++ assets/index-4d67f37b.js
@@ -0,0 +1,1 @@
+import{az as t}from"./index-5ee539c0.js";function i(){return t.jsx("div",{children:"setting"})}export{i as default};
